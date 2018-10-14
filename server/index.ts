a8554584e33dbf4f87dbc1555c7f37988fc13955
Ethan Jon <e10jc@users.noreply.github.com>
2018-10-14 16:27:34 -0700

    custom express server

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,16 @@
-import { createServer } from 'http'
-import { parse } from 'url'
+import * as express from 'express'
 import * as next from 'next'
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const nextApp = next({dev})
+const handle = nextApp.getRequestHandler()
 
-app.prepare()
+nextApp.prepare()
 .then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+  const app = express()
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
-  })
-  .listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
+  app.get('*', (req, res) => handle(req, res))
+    
+  app.listen(port)
 })
