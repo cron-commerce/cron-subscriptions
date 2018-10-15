08b02dc2c45c48d78fd454a1c5190044f6b6b06d
Ethan Jon <e10jc@users.noreply.github.com>
2018-10-15 05:59:12 -0700

    switched to koa and shopify's koa library

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,9 @@
-import * as express from 'express'
-import * as morgan from 'morgan'
+import 'isomorphic-fetch'
+import * as Koa from 'koa'
+import * as Router from 'koa-router'
+import * as session from 'koa-session'
 import * as next from 'next'
-
-import shopifyAppRoute from './shopify-app'
-import shopifyAppConfirmInstallRoute from './shopify-app/confirm-install'
+import shopifyAuth, {verifyRequest} from '@shopify/koa-shopify-auth'
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -12,12 +12,42 @@ const handle = nextApp.getRequestHandler()
 
 nextApp.prepare()
 .then(() => {
-  const app = express()
+  const app = new Koa()
+  app.keys = [process.env.SHOPIFY_APP_SECRET]
 
-  app.use(morgan('dev'))
-  app.get('/shopify-app', shopifyAppRoute())
-  app.get('/shopify-app/confirm-install', shopifyAppConfirmInstallRoute())
-  app.get('*', (req, res) => handle(req, res))
-    
-  app.listen(port)
+  app
+    .use(session(app))
+    .use(
+      shopifyAuth({
+        prefix: '/shopify',
+        apiKey: process.env.SHOPIFY_APP_KEY,
+        secret: process.env.SHOPIFY_APP_SECRET,
+        scopes: ['read_products'],
+        afterAuth(ctx) {
+          const {shop, accessToken} = ctx.session
+          console.log('We did it!', accessToken)
+          ctx.redirect('/')
+        },
+      }),
+    )
+    .use(verifyRequest({
+      authRoute: '/shopify/auth', 
+      fallbackRoute: '/shopify/auth',
+    }))
+    .use((ctx) => {
+      ctx.body = `
+      <script src="https://cdn.shopify.com/s/assets/external/app.js"></script>
+
+      <script type="text/javascript">
+        ShopifyApp.init({
+          apiKey: "${process.env.SHOPIFY_APP_KEY}",
+          shopOrigin: "https://${ctx.session.shop}",
+          debug: true,
+          forceRedirect: true
+        });
+      </script>`
+    })  
+    .listen(port, () => {
+      console.log(`> Ready on http://localhost:${port}`)
+    })
 })
