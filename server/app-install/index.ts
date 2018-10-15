da640d11d8cf370fa88ae9dac6c02e0180b369f3
Ethan Jon <e10jc@users.noreply.github.com>
2018-10-14 20:10:11 -0700

    added app install and app install confirmation routes

---
+++ server/app-install/index.ts
@@ -0,0 +1,21 @@
+import {Request, Response} from 'express'
+
+export default () => (req: Request, res: Response) => {
+  if (!req.query.shop) return res.send('Missing ?shop=')
+
+  const scope = [
+    // 'read_customers',
+    // 'read_orders',
+    'read_products',
+    // 'write_customers',
+    // 'write_orders',
+    // 'write_script_tags',
+  ].join(',')
+
+  return res.redirect([
+    `https://${req.query.shop}/admin/oauth/authorize?client_id=${process.env.SHOPIFY_APP_KEY}`,
+    `scope=${scope}`,
+    `redirect_uri=${process.env.SHOPIFY_APP_HOST}/app-install/confirm`,
+    `state=${process.env.SHOPIFY_APP_NONCE}`,
+  ].join('&'))
+}
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,9 @@
 import * as express from 'express'
 import * as next from 'next'
 
+import appInstallRoute from './app-install/'
+import appInstallConfirmRoute from './app-install/confirm'
+
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({dev})
@@ -10,6 +13,8 @@ nextApp.prepare()
 .then(() => {
   const app = express()
 
+  app.get('/app-install', appInstallRoute())
+  app.get('/app-install/confirm', appInstallConfirmRoute())
   app.get('*', (req, res) => handle(req, res))
     
   app.listen(port)
